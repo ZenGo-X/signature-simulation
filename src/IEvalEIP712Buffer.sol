f09bf9fe6327bd6eee7fe1c13017db0ea8a5b667
Roi Vazan <roi.vazan@kzencorp.com>
2023-01-03 17:39:04 +0200

    added eip folder

--- src/IEvalEIP712Buffer.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
-
-interface IEvalEIP712Buffer {
-    function evalEIP712Buffer(bytes memory signature) external view returns (string[] memory);
-}
