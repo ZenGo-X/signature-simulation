19e669f2e7094984bfe5287b4476cbb726f9ae1b
Vazi <106611793+DeVaz1@users.noreply.github.com>
2023-01-26 15:05:32 +0200

    Update SeaPortMock.sol

--- implementation/src/SeaPort/SeaPortMock.sol
+++ implementation/src/SeaPort/SeaPortMock.sol
@@ -18,7 +18,7 @@ contract SeaPortMock {
     // SeaPort logic
 
     function evalEIP712Buffer(
-        IEvalEIP712Buffer.Domain memory domain,
+        bytes32 domainSeparator,
         string memory primaryType,
         bytes memory encodedSignature
     ) public view returns (string[] memory) {
@@ -26,22 +26,11 @@ contract SeaPortMock {
             keccak256(abi.encodePacked(primaryType)) == keccak256(abi.encodePacked("OrderComponents")),
             "SeaPortMock: Invalid primary type"
         );
-        require(
-            _domainSeparatorV4()
-                == keccak256(
-                    abi.encode(
-                        TYPE_HASH,
-                        keccak256(bytes(domain.name)),
-                        keccak256(bytes(domain.version)),
-                        domain.chainId,
-                        domain.verifyingContract
-                    )
-                ) , "SeaPortMock: Invalid domain"
-        );
+        require(domainSeparator == DOMAIN_SEPARATOR(), "SeaPortMock: Invalid domain");
         return SeaPort712ParserHelper(eip712TransalatorContract).parseSig(encodedSignature);
     }
 
-    function _domainSeparatorV4() internal view returns (bytes32) {
+    function DOMAIN_SEPARATOR() public view returns (bytes32) {
         // prettier-ignore
         return keccak256(
             abi.encode(TYPE_HASH, keccak256(bytes("Seaport")), keccak256(bytes("1.1")), block.chainid, address(this))
