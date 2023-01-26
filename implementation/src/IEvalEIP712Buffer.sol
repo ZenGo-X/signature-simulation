def5f2a08a8973421ad01fde2c0f3551c561adc0
Vazi <106611793+DeVaz1@users.noreply.github.com>
2023-01-26 15:03:41 +0200

    Update IEvalEIP712Buffer.sol

--- implementation/src/IEvalEIP712Buffer.sol
+++ implementation/src/IEvalEIP712Buffer.sol
@@ -2,14 +2,7 @@
 pragma solidity ^0.8.17;
 
 interface IEvalEIP712Buffer {
-    struct Domain {
-        string name;
-        string version;
-        uint256 chainId;
-        address verifyingContract;
-    }
-
-    function evalEIP712Buffer(Domain memory domain, string memory primaryType, bytes memory signature)
+    function evalEIP712Buffer(bytes32 domainHash, string memory primaryType, bytes memory signature)
         external
         view
         returns (string[] memory);
